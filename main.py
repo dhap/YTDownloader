cf2ee1d6788cafd726b278d0e10a3a00e2337b0a
Ivan Krivushchenko <draphap@gmail.com>
2023-03-30 20:40:01 +0200

    Add progress bar for download

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from pytube import YouTube
+from pytube.cli import on_progress
 
-yt = YouTube('https://www.youtube.com/watch?v=xbNzZMfDF-8')
+yt = YouTube('https://www.youtube.com/watch?v=xbNzZMfDF-8',on_progress_callback=on_progress)
 
 print(yt.title)
 print(yt.streams)
