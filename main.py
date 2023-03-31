031983a2a2ebf7cbf558a585569592add7bcd08a
Ivan Krivushchenko <draphap@gmail.com>
2023-03-31 18:22:48 +0200

    Add usrl as argv or imput url after run

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 from pytube import YouTube
 from pytube.cli import on_progress
+from sys import argv
 
-yt = YouTube('https://www.youtube.com/watch?v=xbNzZMfDF-8',on_progress_callback=on_progress)
+
+try:
+    url = argv[1]
+except IndexError as e:
+    print("Input the YouTube  video URL")
+    url = str(input())
+
+#if  len(argv) < 2:
+#    print("Input the YouTube  video URL")
+#    url = str(input())
+#else:
+#    url = str(argv)
+
+yt = YouTube(url, on_progress_callback=on_progress)
 
 print(yt.title)
-print(yt.streams)
+print(yt.streams.filter(progressive=True))
 yd = yt.streams.get_highest_resolution()
 
 yd.download('c:\\Users\\draph\\YtDowloader\\')
+input()
