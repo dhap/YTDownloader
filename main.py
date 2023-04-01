358ef0f8c4c2be5cbb54646c474bce8a5cccddce
Ivan Krivushchenko <draphap@gmail.com>
2023-04-01 15:08:37 +0200

    Change ffmpeg to moviepy, but it can work only .mp4 movie file

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ from pytube import YouTube
 from pytube.cli import on_progress
 from sys import argv
 import ffmpeg
+from moviepy.editor import VideoFileClip, AudioFileClip, concatenate_videoclips
 import os
-
-
 
 needaudio = False
 itag = int()
@@ -28,30 +27,46 @@ itag = int(input('Enter the itag number  '))
 
 
 yd = yt.streams.get_by_itag(itag)
-print(yd, "Size in Mb ", yd.filesize_mb)
+print(yd, "Size - ", yd.filesize_mb, " Mb")
 
 if yd.is_dash:
-    print("need audio track")
+    print("need to download audio track for this choice")
     for a in yt.streams.filter(only_audio=any):
-        print(a, "Size in Mb ", a.filesize_mb)
+        print(a, "Size - ", a.filesize_mb, " Mb")
 
     itaga = int(input('Enter the itag number  '))
     yda = yt.streams.get_by_itag(itaga)
     needaudio = True
 
 try:
-    yd.download()
+    yd.download(".\\video")
 except AttributeError as e:
     print("enter correct number of itag")
 
 if needaudio:
-    
     try:
-        yda.download(".\\audio")    
+        yda.download("\\audio")
     except AttributeError as e:
         print("enter correct number of itag")
+    svideo = str(yd.download("\\video"))
+    pathToSaveOutputVideo = svideo.replace("\\video","")
+    saudio = str(yda.download("\\audio")) 
 
-    input_video = ffmpeg.input(yd.download())
-   
-    input_audio = ffmpeg.input(yda.download(".\\audio"))
-    ffmpeg.concat(input_video, input_audio, v=1, a=1).output('1.mp4').run()
+    # Open the video and audio
+    video_clip = VideoFileClip(svideo)
+    audio_clip = AudioFileClip(saudio)
+
+
+
+    # Concatenate the video clip with the audio clip
+    final_clip = video_clip.set_audio(audio_clip)
+
+    # Export the final video with audio
+    final_clip.write_videofile(title + ".mp4")
+
+
+'''
+    input_video = ffmpeg.input(svideo)
+    input_audio = ffmpeg.input(saudio)
+    ffmpeg.concat(input_video, input_audio, v=1, a=1).output(pathToSaveOutputVideo).run()
+'''
