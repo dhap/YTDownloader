8caa6acbd6b7dbc10da9d0d894272445e00a28d2
Ivan Krivushchenko <draphap@gmail.com>
2023-04-01 17:30:40 +0200

    Add URL validation

--- main.py
+++ main.py
@@ -1,72 +1,77 @@
 from pytube import YouTube
 from pytube.cli import on_progress
 from sys import argv
-import ffmpeg
 from moviepy.editor import VideoFileClip, AudioFileClip, concatenate_videoclips
-import os
+import requests
 
 needaudio = False
 itag = int()
 
+#Check argument
 try:
     url = argv[1]
 except IndexError as e:
+    print("No have argument")
+
+#Validation URL
+while True:
     print("Input the YouTube video URL")
     url = str(input())
-
-yt = YouTube(url, on_progress_callback=on_progress)
+    if url.count("https://www.youtube.com") > 0:
+        r = requests.get(url) # random video id
+        if 'Video unavailable' in r.text:
+            print("Video unavailable")
+            continue
+        else:
+            yt = YouTube(url, on_progress_callback=on_progress)
+            break
+    else:
+        print("This is not YouTube URL")
+        continue
 
 title = str(yt.title)
 title = title.replace("//",'')
 print(title)
 
 for a in yt.streams.filter(mime_type="video/mp4"):
-    print(a, "Size in Mb ", a.filesize_mb)
+    print(a, "Size - ", a.filesize_mb, "Mb")
 
 itag = int(input('Enter the itag number  '))
 
-
+#Select id video stream
 yd = yt.streams.get_by_itag(itag)
-print(yd, "Size - ", yd.filesize_mb, " Mb")
+print("Selected stream - ", yd, "Size - ", yd.filesize_mb, "Mb")
 
+#Chek if it is only video stresm without audio line
 if yd.is_dash:
     print("need to download audio track for this choice")
     for a in yt.streams.filter(only_audio=any):
-        print(a, "Size - ", a.filesize_mb, " Mb")
+        print(a, "Size - ", a.filesize_mb, "Mb")
 
     itaga = int(input('Enter the itag number  '))
     yda = yt.streams.get_by_itag(itaga)
     needaudio = True
 
 try:
-    yd.download(".\\video")
+    yd.download("\\YTD_video")
 except AttributeError as e:
-    print("enter correct number of itag")
+    print("Enter correct number of itag")
 
 if needaudio:
     try:
-        yda.download("\\audio")
+        yda.download("\\YTD_audio")
     except AttributeError as e:
         print("enter correct number of itag")
-    svideo = str(yd.download("\\video"))
-    pathToSaveOutputVideo = svideo.replace("\\video","")
-    saudio = str(yda.download("\\audio")) 
+    svideo = str(yd.download("\\YTD_video"))
+    pathToSaveOutputVideo = svideo.replace("\\YTD_video","")
+    saudio = str(yda.download("\\YTD_audio")) 
 
     # Open the video and audio
     video_clip = VideoFileClip(svideo)
     audio_clip = AudioFileClip(saudio)
-
-
 
     # Concatenate the video clip with the audio clip
     final_clip = video_clip.set_audio(audio_clip)
 
     # Export the final video with audio
     final_clip.write_videofile(title + ".mp4")
-
-
-'''
-    input_video = ffmpeg.input(svideo)
-    input_audio = ffmpeg.input(saudio)
-    ffmpeg.concat(input_video, input_audio, v=1, a=1).output(pathToSaveOutputVideo).run()
-'''
