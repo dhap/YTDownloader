1122183cfe082b7022048fbc996836289b813607
Ivan Krivushchenko <draphap@gmail.com>
2023-04-02 09:55:20 +0200

    few little changes

--- main.py
+++ main.py
@@ -4,9 +4,36 @@ from sys import argv
 from moviepy.editor import VideoFileClip, AudioFileClip, concatenate_videoclips
 import requests
 
-needaudio = False
 url = str()
 itag = int()
+
+#If stream without audio line
+def needaudiosplit():
+    try:
+        yda.download("\\YTD_audio")
+    except AttributeError as e:
+        print("enter correct number of itag")
+
+    svideo = str(yd.download("\\YTD_video"))
+    pathToSaveOutputVideo = svideo.replace("\\YTD_video","")
+    saudio = str(yda.download("\\YTD_audio")) 
+
+    # Open the video and audio
+    video_clip = VideoFileClip(svideo)
+    audio_clip = AudioFileClip(saudio)
+
+    # Concatenate the video clip with the audio clip
+    final_clip = video_clip.set_audio(audio_clip)
+
+    # Export the final video with audio
+    final_clip.write_videofile(title + ".mp4")
+
+def validateItag(inputItag, streamsIn):
+    for a in streamsIn:
+        if inputItag in str(a):
+            return True
+        else:
+            return False
 
 #Check argument
 try:
@@ -16,22 +43,17 @@ except IndexError as e:
 
 #Validation URL
 while True:
-    if url == "":
-        print("Input the YouTube video URL")
-        url = str(input())
     if url.count("https://www.youtube.com") > 0:
-        r = requests.get(url) # random video id
+        r = requests.get(url) 
         if 'unavailable_video.png' in r.text:
-            print("Video unavailable")
-            print("Input the YouTube video URL")
+            print("This is not YouTube URL, Input the YouTube video URL")
             url = str(input())
             continue
         else:
             yt = YouTube(url, on_progress_callback=on_progress)
             break
     else:
-        print("This is not YouTube URL")
-        print("Input the YouTube video URL")
+        print("This is not YouTube URL, Input the YouTube video URL")
         url = str(input())
         continue
 
@@ -39,8 +61,9 @@ title = str(yt.title)
 title = title.replace("//",'')
 print(title)
 
+#Output streams
 for a in yt.streams.filter(mime_type="video/mp4"):
-    print(a, "Size - ", a.filesize_mb, "Mb")
+    print(a, "    Size =", a.filesize_mb, "Mb")
 
 itag = int(input('Enter the itag number  '))
 
@@ -56,28 +79,9 @@ if yd.is_dash:
 
     itaga = int(input('Enter the itag number  '))
     yda = yt.streams.get_by_itag(itaga)
-    needaudio = True
+    needaudiosplit()
 
 try:
     yd.download()
 except AttributeError as e:
     print("Enter correct number of itag")
-
-if needaudio:
-    try:
-        yda.download("\\YTD_audio")
-    except AttributeError as e:
-        print("enter correct number of itag")
-    svideo = str(yd.download("\\YTD_video"))
-    pathToSaveOutputVideo = svideo.replace("\\YTD_video","")
-    saudio = str(yda.download("\\YTD_audio")) 
-
-    # Open the video and audio
-    video_clip = VideoFileClip(svideo)
-    audio_clip = AudioFileClip(saudio)
-
-    # Concatenate the video clip with the audio clip
-    final_clip = video_clip.set_audio(audio_clip)
-
-    # Export the final video with audio
-    final_clip.write_videofile(title + ".mp4")
